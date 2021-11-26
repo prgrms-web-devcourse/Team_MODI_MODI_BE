ac9dd9903b2a6bbaa980c8c95ff6488115e31e3e
Sangmin Lee <sangminlee7648@gmail.com>
2021-11-26 22:01:20 +0900

    [MODI-9] FEAT : 보일러플레이트 생성

---
+++ src/main/java/com/prgrms/modi/error/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.prgrms.modi.error.exception;
+
+import javax.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+
+}
