282a4b4808354e743eaa96bff67c547699f63e72
suhyunsim <cnc1106@naver.com>
2021-12-08 18:42:11 +0900

    [MODI-85] FEAT:  포인트 응답용 DTO 생성

---
+++ src/main/java/com/prgrms/modi/user/dto/PointAmountDto.java
@@ -0,0 +1,14 @@
+package com.prgrms.modi.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class PointAmountDto {
+
+    private Integer points;
+
+    public PointAmountDto(Integer points) {
+        this.points = points;
+    }
+
+}
