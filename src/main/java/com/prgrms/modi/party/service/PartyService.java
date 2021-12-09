cccf80945545b34d65984e782044d5173d82789f
sirin0762 <tlsdydwls123@gmail,com>
2021-12-09 18:27:40 +0900

    [MODI-139] DOCS : Swagger 문서화 - 제가 구현한 controller 메서드에 swagger 관련 처리를 했습니다. - 중간에 응답이 잘못된 부분이 발견되어 해당 응답을 수정했습니다.

--- src/main/java/com/prgrms/modi/party/service/PartyService.java
+++ src/main/java/com/prgrms/modi/party/service/PartyService.java
@@ -89,7 +89,7 @@ public class PartyService {
     }
 
     @Transactional
-    public Long joinParty(Long userId, Long partyId) {
+    public PartyIdResponse joinParty(Long userId, Long partyId) {
         User user = memberService.findUser(userId);
         Party party = this.findPartyWithOtt(partyId);
 
@@ -98,7 +98,7 @@ public class PartyService {
         party.increaseMonthlyReimbursement(party.getOtt().getMonthlyFee());
         party.increaseRemainingReimbursement(party.getTotalFee());
         memberService.save(party, user);
-        return partyId;
+        return PartyIdResponse.from(party);
     }
 
     private Party findPartyWithOtt(Long partyId) {
