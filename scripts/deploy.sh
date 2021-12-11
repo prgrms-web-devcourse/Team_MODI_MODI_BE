8d4eed3e5d4a5edd46a444857c662b12ce8bb131
suhyunsim <cnc1106@naver.com>
2021-12-11 21:42:49 +0900

    [MODI-126] CHORE:  로그 파일 경로 명명

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일명: $JAR_NAME" >>/home/ubuntu/action/deploy.log
 echo "> 현재 구동 중인 애플리케이션 pid 확인" >>/home/ubuntu/action/deploy.log
 CURRENT_PID=$(ps -e | grep java | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID" >>/home/ubuntu/action/deploy.log
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >>/home/ubuntu/action/deploy.log
@@ -21,11 +21,11 @@ echo "> 새 어플리케이션 배포" >>/home/ubuntu/action/deploy.log
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME에 실행권한 추가"
+echo "> $JAR_NAME에 실행권한 추가" >>/home/ubuntu/action/deploy.log
 
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행"
+echo "> $JAR_NAME 실행" >>/home/ubuntu/action/deploy.log
 
 nohup java -jar -Dspring.profiles.active=prod $JAR_NAME >/home/ubuntu/action/build/libs/nohup.out 2>&1 &
 
