8e27821498902c9425570fc369228b7b8573904c
Sangmin Lee <sangminlee7648@gmail.com>
2021-12-14 13:39:31 +0900

    CHORE:flyway 반복스크립트 레코드 중복 문제 해결

--- src/main/resources/db/seed/R__seed.sql
+++ src/main/resources/db/seed/R__seed.sql
@@ -1,4 +1,5 @@
-INSERT INTO users(id, username, role, date_of_birth, points, created_at, updated_at, deleted_at, provider, provider_id)
+INSERT
+IGNORE INTO users(id, username, role, date_of_birth, points, created_at, updated_at, deleted_at, provider, provider_id)
 VALUES
     (1, '테스트 유저1', 'USER', '1997-01-13', 50000, '2021-11-01 06:30:00', '2021-11-01 06:30:00', null, null, null),
     (2, '테스트 유저2', 'USER', '1997-02-13', 0, '2021-11-02 06:30:00', '2021-11-02 06:30:00', null, null, null),
@@ -6,7 +7,8 @@ VALUES
     (4, '테스트 유저4', 'USER', '1997-04-13', 0, '2021-11-04 06:30:00', '2021-11-04 06:30:00', null, null, null),
     (5, '테스트 유저5', 'ADMIN', '1997-05-13', 0, '2021-11-05 06:30:00', '2021-11-05 06:30:00', null, null, null);
 
-INSERT INTO otts(id, name, english_name, subscription_fee, monthly_price, max_member_capacity, grade, created_at, updated_at)
+INSERT
+IGNORE INTO otts(id, name, english_name, subscription_fee, monthly_price, max_member_capacity, grade, created_at, updated_at)
 VALUES
     (1, '넷플릭스', 'netflix', 10000, 2500, 4, '프리미엄', '2021-11-01 06:30:00', '2021-11-01 06:30:00'),
     (2, '디즈니 플러스', 'disneyPlus', 10000, 2500, 4, '프리미엄', '2021-11-01 06:30:00', '2021-11-01 06:30:00'),
@@ -14,7 +16,8 @@ VALUES
     (4, '왓챠', 'watcha', 10000, 2500, 4, '프리미엄', '2021-11-01 06:30:00', '2021-11-01 06:30:00'),
     (5, '티빙', 'tving', 10000, 2500, 4, '프리미엄', '2021-11-01 06:30:00', '2021-11-01 06:30:00');
 
-INSERT INTO parties(id, party_member_capacity, current_member, total_price, monthly_reimbursement, remaining_reimbursement, start_date, end_date,
+INSERT
+INGORE INTO parties(id, party_member_capacity, current_member, total_price, monthly_reimbursement, remaining_reimbursement, start_date, end_date,
                     period, must_filled, shared_id, shared_password_encrypted, status, created_at, updated_at, deleted_at, ott_id)
 VALUES
     (1, 4, 4, 10000, 5000, 25000, '2021-11-02', '2022-05-02', 6, 1, 'modi112@gmail.com', 'modi', 'ONGOING', '2021-11-02 06:30:00', '2021-11-01 06:30:00', null, 1),
@@ -25,13 +28,15 @@ VALUES
     (6, 4, 2, 100000, 0, 0, '2021-12-26', '2022-05-02', 5, 1, 'modi112@gmail.com', 'modi', 'RECRUITING', '2021-11-02 06:30:00', '2021-11-01 06:30:00', null, 1),
     (7, 4, 2, 100000, 0, 0, '2021-12-26', '2022-05-02', 5, 1, 'modi112@gmail.com', 'modi', 'RECRUITING', '2021-11-02 06:30:00', '2021-11-01 06:30:00', '2021-11-02 06:30:00', 1);
 
-INSERT INTO rules(id, name, created_at, updated_at)
+INSERT
+IGNORE INTO rules(id, name, created_at, updated_at)
 VALUES
     (1, '1인 1회선', '2021-11-01 06:30:00', '2021-11-01 06:30:00'),
     (2, '1인 2회선', '2021-11-01 06:30:00', '2021-11-01 06:30:00'),
     (3, '양도 금지', '2021-11-01 06:30:00', '2021-11-01 06:30:00');
 
-INSERT INTO party_rule(party_id, rule_id)
+INSERT
+IGNORE INTO party_rule(party_id, rule_id)
 VALUES
     (1, 1),
     (1, 2),
@@ -39,7 +44,8 @@ VALUES
     (2, 1),
     (2, 2);
 
-INSERT INTO members(id, is_leader, created_at, updated_at, deleted_at, party_id, user_id)
+INSERT
+IGNORE INTO members(id, is_leader, created_at, updated_at, deleted_at, party_id, user_id)
 VALUES
     (1, 1, '2021-11-02 06:30:00', '2021-11-02 06:30:00', null, 1, 1),
     (2, 0, '2021-11-02 06:30:00', '2021-11-02 06:30:00', null, 1, 2),
