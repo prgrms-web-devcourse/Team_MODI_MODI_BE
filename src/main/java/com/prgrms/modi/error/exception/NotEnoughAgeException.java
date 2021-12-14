3816ea4e9b8f2d6b29d1fc8b56677e6454dfd700
suhyunsim <cnc1106@naver.com>
2021-12-14 22:37:12 +0900

    [MODI-105] FEAT:  나이 제한 예외, 생년월일 못 받아올 때의 예외 추가

---
+++ src/main/java/com/prgrms/modi/error/exception/NotEnoughAgeException.java
@@ -0,0 +1,9 @@
+package com.prgrms.modi.error.exception;
+
+public class NotEnoughAgeException extends RuntimeException {
+
+    public NotEnoughAgeException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/prgrms/modi/error/exception/NotEnoughUserInformationException.java
@@ -0,0 +1,9 @@
+package com.prgrms.modi.error.exception;
+
+public class NotEnoughUserInformationException extends RuntimeException {
+
+    public NotEnoughUserInformationException(String message) {
+        super(message);
+    }
+
+}
