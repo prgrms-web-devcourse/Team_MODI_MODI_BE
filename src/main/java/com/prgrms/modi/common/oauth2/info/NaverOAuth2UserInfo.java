f74dd746298fc5a82ba5eb41f0f0d29d5a280d6b
suhyunsim <cnc1106@naver.com>
2021-12-19 13:42:57 +0900

    [MODI-244] REFACTOR: 나이 관련 인증 실패 시 필수정보 부족 예외처리
    - 인증 실패 유저는 저장되지 않도록 수정

--- src/main/java/com/prgrms/modi/common/oauth2/info/NaverOAuth2UserInfo.java
+++ src/main/java/com/prgrms/modi/common/oauth2/info/NaverOAuth2UserInfo.java
@@ -5,10 +5,14 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NaverOAuth2UserInfo extends OAuth2UserInfo {
 
     private static final int ADULT_AGE = 19;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     public NaverOAuth2UserInfo(Map<String, Object> attributes) {
         super(attributes);
@@ -25,7 +29,9 @@ public class NaverOAuth2UserInfo extends OAuth2UserInfo {
     private String getBirthDate() {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
-        if (response == null) {
+        log.info("[*] response: {}", response);
+
+        if (response.get("birthyear") == null) {
             throw new NotEnoughUserInformationException("유저의 필수 정보가 부족합니다");
         }
 
