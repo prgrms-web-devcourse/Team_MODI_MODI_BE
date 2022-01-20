ebb7add6694d9e282a37e7f86f38f90d62e97e8e
suhyunsim <cnc1106@naver.com>
2022-01-20 23:25:13 +0900

    [MODI-293] REFACTOR: 삭제된 멤버의 알림 조회하지 않도록 수정

--- src/main/java/com/prgrms/modi/user/repository/MemberRepository.java
+++ src/main/java/com/prgrms/modi/user/repository/MemberRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findMembersByUserId(Long userId);
+    List<Member> findMembersByUserIdAndDeletedAtIsNull(Long userId);
 
 }
